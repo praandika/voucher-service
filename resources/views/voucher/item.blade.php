325805fa6546c2db8325c8a7c10da3554732359e
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-11 12:30:51 +0800

    update item blade

--- resources/views/voucher/item.blade.php
+++ resources/views/voucher/item.blade.php
@@ -2,14 +2,22 @@
 @section('title','Your Voucher')
 
 @section('content')
-<div class="d-flex justify-content-center">
+<div class="d-flex justify-content-center" style="position: relative;">
     <!-- QR Code -->
-    @forelse($data as $o)
-    <p style="text-align: center;">{!! QrCode::size(200)->generate('localhost/scanned/'.$o->code) !!}</p>
-    @empty
-    <p style="text-align: center;">voucher's code doesn't exist</p>
-    @endforelse
+    <div class="voucher" style="margin-top: 15px; background: linear-gradient(175deg, rgba(0,93,207,1) 0%, rgba(0,255,209,1) 100%); padding: 15px; border-radius: 20px; position: relative; height: 705px;">
+        @forelse($data as $o)
+        <p style="text-align: center;">{!! QrCode::size(300)->generate('localhost/scanned/'.$o->code) !!}</p>
+        @empty
+        <p style="text-align: center;">voucher's code doesn't exist</p>
+        @endforelse
     <!-- END QR Code -->
+
+    <img src="{{ asset('img/discount.png') }}" alt="Discount Service" srcset="" width="300" style="margin-top: -5px; border-radius: 20px;">
+
+    <div class="value" style="position: relative; bottom: 160px;">
+        <p style="font-size: 70px; text-align: center; font-weight: bold; color: #EFDAD7;">20%</p>
+    </div>
+    </div>
 
     
 </div>
