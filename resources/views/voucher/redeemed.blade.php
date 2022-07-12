badc7fdb2006ee7804e007f7e261125248ed7265
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-12 14:29:19 +0800

    edit redeemed page

--- resources/views/voucher/redeemed.blade.php
+++ resources/views/voucher/redeemed.blade.php
@@ -6,7 +6,7 @@
     <div class="redeemed" style="position: relative;">
         <img src="{{ asset('img/redeemed.png') }}" alt="Voucher Redeemed" style="border-radius: 20px; margin: 0 auto; margin-top: 100px;" width="300px">
 
-        <div class="data" style="position: relative; text-align: center; bottom: 150px; color: #D9D9D9; font-size: 20px;">
+        <div class="data" style="position: relative; text-align: center; bottom: 180px; color: #D9D9D9; font-size: 20px;">
             @forelse($data as $o)
                 <p style="color: #CCCACA; font-weight: bold;">{{ $o->plate_no }}</p>
                 <p style="color: #CCCACA;">{{ $o->name }}</p>
