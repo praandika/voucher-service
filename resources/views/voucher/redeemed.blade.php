485670629d51348c80eb146fcb956d101b164981
Andika Pranayoga <praandikayoga@gmail.com>
2022-07-12 14:34:40 +0800

    edit redeemed page

--- resources/views/voucher/redeemed.blade.php
+++ resources/views/voucher/redeemed.blade.php
@@ -6,14 +6,14 @@
     <div class="redeemed" style="position: relative;">
         <img src="{{ asset('img/redeemed.png') }}" alt="Voucher Redeemed" style="border-radius: 20px; margin: 0 auto; margin-top: 100px;" width="300px">
 
-        <div class="data" style="position: relative; text-align: center; bottom: 180px; color: #D9D9D9; font-size: 20px;">
+        <div class="data" style="position: relative; text-align: center; bottom: 150px; color: #D9D9D9; font-size: 20px;">
             @forelse($data as $o)
-                <p style="color: #CCCACA; font-weight: bold;">
-                    {{ $o->plate_no }} <br>
-                    {{ $o->name }}
+                <p>
+                    <span style="color: #CCCACA; font-weight: bold;">{{ $o->plate_no }}</span> <br>
+                    <span style="color: #CCCACA;">{{ $o->name }}</span> <br>
+                    <p>{{ $o->phone }}</p> <br>
+                    <p>{{ $o->status }}</p>
                 </p>
-                <p>{{ $o->phone }}</p>
-                <p>{{ $o->status }}</p>
             @empty
                 <p>voucher's code doesn't exist</p>
             @endforelse
